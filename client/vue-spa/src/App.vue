40b6ce39cdf9106673e63daf07ca2b9f754b6c03
kyainuma <yasu6491@icloud.com>
2023-03-31 12:08:53 +0900

    SPAとAPIの連携

--- client/vue-spa/src/App.vue
+++ client/vue-spa/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Yamabiko/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Yamabiko from './components/Yamabiko.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Yamabiko
   }
 }
 </script>
